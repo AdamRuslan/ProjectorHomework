a8cc4cd118cfbb100976e4408bc47980d14f2d07
AdamRuslan <129087273+AdamRuslan@users.noreply.github.com>
2023-06-25 08:38:05 -0400

    Update -- Users table.py

--- -- Users table.py
+++ -- Users table.py
@@ -1,6 +1,6 @@
 -- Users table
 CREATE TABLE Users (
-    user_id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     username TEXT,
     password TEXT,
     email TEXT,
@@ -13,65 +13,66 @@ CREATE TABLE Users (
 
 -- Hosts table
 CREATE TABLE Hosts (
-    host_id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
     bio TEXT,
     profile_picture TEXT,
     location TEXT,
-    FOREIGN KEY (host_id) REFERENCES Users (user_id)
+    FOREIGN KEY (user_id) REFERENCES Users (id)
 );
 
 -- Guests table
 CREATE TABLE Guests (
-    guest_id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
     profile_picture TEXT,
     location TEXT,
-    FOREIGN KEY (guest_id) REFERENCES Users (user_id)
+    FOREIGN KEY (user_id) REFERENCES Users (id)
 );
 
 -- Rooms table
 CREATE TABLE Rooms (
-    room_id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     host_id INTEGER,
     room_type TEXT,
     description TEXT,
     capacity INTEGER,
     price_per_night REAL,
-    has_ac INTEGER,
-    has_refrigerator INTEGER,
-    FOREIGN KEY (host_id) REFERENCES Hosts (host_id)
+    is_ac_available BOOLEAN,
+    has_refrigerator BOOLEAN,
+    FOREIGN KEY (host_id) REFERENCES Hosts (id)
 );
 
 -- Reservations table
 CREATE TABLE Reservations (
-    reservation_id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     guest_id INTEGER,
     room_id INTEGER,
     check_in_date TEXT,
     check_out_date TEXT,
     payment_status TEXT,
-    FOREIGN KEY (guest_id) REFERENCES Guests (guest_id),
-    FOREIGN KEY (room_id) REFERENCES Rooms (room_id)
+    FOREIGN KEY (guest_id) REFERENCES Guests (id),
+    FOREIGN KEY (room_id) REFERENCES Rooms (id)
 );
 
 -- Reviews table
 CREATE TABLE Reviews (
-    review_id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     guest_id INTEGER,
-    host_id INTEGER,
     reservation_id INTEGER,
     rating INTEGER,
     comment TEXT,
-    FOREIGN KEY (guest_id) REFERENCES Guests (guest_id),
-    FOREIGN KEY (host_id) REFERENCES Hosts (host_id),
-    FOREIGN KEY (reservation_id) REFERENCES Reservations (reservation_id)
+    FOREIGN KEY (guest_id) REFERENCES Guests (id),
+    FOREIGN KEY (reservation_id) REFERENCES Reservations (id)
 );
 
 -- Payments table (optional for guest)
 CREATE TABLE Payments (
-    payment_id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     reservation_id INTEGER,
     payment_date TEXT,
     amount REAL,
     payment_method TEXT,
-    FOREIGN KEY (reservation_id) REFERENCES Reservations (reservation_id)
+    FOREIGN KEY (reservation_id) REFERENCES Reservations (id)
 );
+
