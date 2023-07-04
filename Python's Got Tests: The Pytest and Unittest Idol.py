b212fe3669255f74187b9e8338cc87b0b3e9b340
AdamRuslan <129087273+AdamRuslan@users.noreply.github.com>
2023-07-04 13:05:47 -0400

    Update Python's Got Tests: The Pytest and Unittest Idol.py

--- Python's Got Tests: The Pytest and Unittest Idol.py
+++ Python's Got Tests: The Pytest and Unittest Idol.py
@@ -1,55 +1,33 @@
-# Test Bank Class
-def test_bank_open_account():
+def test_bank_update():
+    # Create test accounts
+    savings_account = SavingsAccount(2000, "SA001", 3)
+    current_account = CurrentAccount(-500, "CA001", 1000)
+
+    # Create a Bank object and open the accounts
     bank = Bank()
+    bank.open_account(savings_account)
+    bank.open_account(current_account)
 
-    # Test opening an account with a positive initial balance
-    account1 = Account.create_account("A001")
-    bank.open_account(account1)
-    assert len(bank.accounts) == 1
-    assert account1.get_balance() == 0.0
+    # Perform the update
+    bank.update()
 
-    account1.deposit(1000)
-    assert account1.get_balance() == 1000.0
+    # Check if the balances are updated correctly for SavingsAccount
+    expected_savings_balance = 2000 + (2000 * 3 / 100)  # Initial balance + interest
+    assert savings_account.get_balance() == expected_savings_balance
 
-    # Test opening an account with a zero initial balance
-    account2 = Account.create_account("A002")
-    bank.open_account(account2)
-    assert len(bank.accounts) == 2
-    assert account2.get_balance() == 0.0
+    # Check if the overdraft notification is displayed for CurrentAccount
+    expected_current_balance = -500
+    assert current_account.get_balance() == expected_current_balance
 
-    # Test opening an account with a negative initial balance
-    try:
-        account3 = Account.create_account("A003")
-        account3.withdraw(100)  # Attempt to withdraw from zero balance
-        bank.open_account(account3)  # This line should raise a ValueError
-        # If the line above doesn't raise an error, the test should fail
-        assert False, "Opening an account with a negative balance should raise a ValueError"
-    except ValueError:
-        assert True
+    # Perform additional transactions to trigger overdraft notification
+    current_account.withdraw(100)
+    bank.update()
 
-    # Test opening multiple accounts
-    account4 = Account.create_account("A004")
-    bank.open_account(account4)
-    account5 = Account.create_account("A005")
-    bank.open_account(account5)
-    assert len(bank.accounts) == 4
+    # Check if the overdraft notification is displayed again after the update
+    expected_current_balance -= 100
+    assert current_account.get_balance() == expected_current_balance
 
-    # Test opening duplicate accounts with the same account number
-    account6 = Account.create_account("A004")
-    bank.open_account(account6)
-    account7 = Account.create_account("A005")
-    bank.open_account(account7)
-    assert len(bank.accounts) == 6
-
-    # Test opening duplicate accounts with different account numbers
-    account8 = Account.create_account("A006")
-    bank.open_account(account8)
-    account9 = Account.create_account("A007")
-    bank.open_account(account9)
-    assert len(bank.accounts) == 8
-
-    print("Bank open_account method tests passed successfully!")
-
+    print("Bank update method tests passed successfully!")
 
 # Run the test
-test_bank_open_account()
+test_bank_update()
