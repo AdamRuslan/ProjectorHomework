958582f22232715468c65d201feef3a07bbbeb2b
AdamRuslan <129087273+AdamRuslan@users.noreply.github.com>
2023-07-04 11:40:46 -0400

    Update Python's Got Tests: The Pytest and Unittest Idol.py

--- Python's Got Tests: The Pytest and Unittest Idol.py
+++ Python's Got Tests: The Pytest and Unittest Idol.py
@@ -3,24 +3,24 @@ def test_open_account():
     bank = Bank()
 
     # Create accounts with different balances
-    account1 = Account.create_account("A001")  # Account with zero balance
-    account2 = Account.create_account("A002")  # Account with positive balance
-    account3 = Account.create_account("A003")  # Account with negative balance
+    zero_balance_account = Account.create_account("A001")  # Account with zero balance
+    positive_balance_account = Account.create_account("A002")  # Account with positive balance
+    negative_balance_account = Account.create_account("A003")  # Account with negative balance
 
     # Open the accounts in the bank
-    bank.open_account(account1)
-    bank.open_account(account2)
-    bank.open_account(account3)
+    bank.open_account(zero_balance_account)
+    bank.open_account(positive_balance_account)
+    bank.open_account(negative_balance_account)
 
     # Verify that the accounts are open and have the correct balances
-    assert account1.get_balance() == 0.0
-    assert account2.get_balance() == 0.0
-    assert account3.get_balance() == 0.0  # Negative balance should be set to zero
+    assert zero_balance_account.get_balance() == 0.0
+    assert positive_balance_account.get_balance() == 0.0
+    assert negative_balance_account.get_balance() == 0.0  # Negative balance should be set to zero
 
     # Verify that the accounts are in the bank's account list
-    assert account1 in bank.accounts
-    assert account2 in bank.accounts
-    assert account3 in bank.accounts
+    assert zero_balance_account in bank.accounts
+    assert positive_balance_account in bank.accounts
+    assert negative_balance_account in bank.accounts
 
     # Verify that the correct number of accounts are opened in the bank
     assert len(bank.accounts) == 3
