f13f772a3cbdb2e87249f36864aae7ede08966b6
AdamRuslan <129087273+AdamRuslan@users.noreply.github.com>
2023-07-04 11:53:27 -0400

    Update Python's Got Tests: The Pytest and Unittest Idol.py

--- Python's Got Tests: The Pytest and Unittest Idol.py
+++ Python's Got Tests: The Pytest and Unittest Idol.py
@@ -1,31 +1,39 @@
-def test_open_account():
-    # Create a Bank object
-    bank = Bank()
+class Bank:
+    def __init__(self):
+        self.accounts = []
 
-    # Create accounts with different balances
-    zero_balance_account = Account.create_account("A001")  # Account with zero balance
-    positive_balance_account = Account(1000, "A002")  # Account with positive balance of 1000
-    negative_balance_account = Account(-500, "A003")  # Account with negative balance of -500
+    def open_account(self, account):
+        self.accounts.append(account)
+        initial_balance = account.get_balance()
+        if initial_balance < 0:
+            account.withdraw(initial_balance)
 
-    # Open the accounts in the bank
-    bank.open_account(zero_balance_account)
-    bank.open_account(positive_balance_account)
-    bank.open_account(negative_balance_account)
 
-    # Verify that the accounts are open and have the correct balances
-    assert zero_balance_account.get_balance() == 0.0
-    assert positive_balance_account.get_balance() == 1000
-    assert negative_balance_account.get_balance() == -500
+# Create a Bank object
+bank = Bank()
 
-    # Verify that the accounts are in the bank's account list
-    assert zero_balance_account in bank.accounts
-    assert positive_balance_account in bank.accounts
-    assert negative_balance_account in bank.accounts
+# Create accounts with different balances
+zero_balance_account = Account.create_account("A001")  # Account with zero balance
+positive_balance_account = Account(1000, "A002")  # Account with positive balance of 1000
+negative_balance_account = Account(-500, "A003")  # Account with negative balance of -500
 
-    # Verify that the correct number of accounts are opened in the bank
-    assert len(bank.accounts) == 3
+# Open the accounts in the bank
+bank.open_account(zero_balance_account)
+bank.open_account(positive_balance_account)
+bank.open_account(negative_balance_account)
 
-    print("open_account test passed.")
+# Verify that the accounts are open and have the correct balances
+assert zero_balance_account.get_balance() == 0.0
+assert positive_balance_account.get_balance() == 1000
+assert negative_balance_account.get_balance() == -500
 
-# Run the test
-test_open_account()
+# Verify that the accounts are in the bank's account list
+assert zero_balance_account in bank.accounts
+assert positive_balance_account in bank.accounts
+assert negative_balance_account in bank.accounts
+
+# Verify that the correct number of accounts are opened in the bank
+assert len(bank.accounts) == 3
+
+print("open_account test passed.")
+
